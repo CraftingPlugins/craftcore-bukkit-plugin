6f92800f7c14b100cf7397693ac94f7d53b301de
LeeGodSRC <lee20040919@gmail.com>
2023-08-04 19:09:09 +0800

    feat: Made CraftCore a bukkit plugin

--- src/main/java/dev/craftplugins/craftcore/CraftCore.java
+++ src/main/java/dev/craftplugins/craftcore/CraftCore.java
@@ -1,21 +1,6 @@
 package dev.craftplugins.craftcore;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import io.fairyproject.plugin.Plugin;
 
-public final class CraftCore extends JavaPlugin {
-
-    public static void require(int major, int minor, int build) {
-        CraftCore plugin = CraftCore.getPlugin(CraftCore.class);
-        String version = plugin.getDescription().getVersion();
-
-        String[] split = version.split("\\.");
-        int currentMajor = Integer.parseInt(split[0]);
-        int currentMinor = Integer.parseInt(split[1]);
-        int currentBuild = Integer.parseInt(split[2]);
-
-        if (currentMajor < major || currentMinor < minor || currentBuild < build) {
-            throw new IllegalStateException("CraftCore version " + major + "." + minor + "." + build + " or higher is required. Please update CraftCore.");
-        }
-    }
-
+public final class CraftCore extends Plugin {
 }
