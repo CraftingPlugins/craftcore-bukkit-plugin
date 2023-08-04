a1567e07063b8cfa2f7378690b0a80241ef7b697
LeeGodSRC <lee20040919@gmail.com>
2023-08-04 15:39:49 +0800

    chore: Bumped fairy version to 0.6.6b4-SNAPSHOT chore: Revamped structure

--- src/main/java/dev/craftplugins/craftcore/CraftCore.java
+++ src/main/java/dev/craftplugins/craftcore/CraftCore.java
@@ -4,14 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CraftCore extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public static void require(int major, int minor, int build) {
+        CraftCore plugin = CraftCore.getPlugin(CraftCore.class);
+        String version = plugin.getDescription().getVersion();
 
+        String[] split = version.split("\\.");
+        int currentMajor = Integer.parseInt(split[0]);
+        int currentMinor = Integer.parseInt(split[1]);
+        int currentBuild = Integer.parseInt(split[2]);
+
+        if (currentMajor < major || currentMinor < minor || currentBuild < build) {
+            throw new IllegalStateException("CraftCore version " + major + "." + minor + "." + build + " or higher is required. Please update CraftCore.");
+        }
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
